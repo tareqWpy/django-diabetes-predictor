0df50080dc94573dc1b7916c3a1e31a15d3a8ef1
tareqWpy <taregh.khaleghi1381@gmail.com>
2024-11-02 18:53:11 +0000

    Resolved STARTTLES issue

--- core/core/settings.py
+++ core/core/settings.py
@@ -178,6 +178,7 @@ EMAIL_BACKEND = config(
 EMAIL_HOST = config("EMAIL_HOST", default="smtp4dev")
 EMAIL_PORT = config("EMAIL_PORT", default=25)
 # the EMAIL_USE_TLS disabled because of development mode
+# must be enable for production mode
 # EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=False)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default=None)
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default=None)
