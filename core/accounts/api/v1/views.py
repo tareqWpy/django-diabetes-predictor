e736a93dc3a20621da3a28e99afc418c967ea05c
tareqWpy <taregh.khaleghi1381@gmail.com>
2024-11-23 18:10:09 +0000

    bug fix

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from ...models import Profile, User, UserType
+from ...models import Profile, User
 from .permissions import IsAuthenticatedAndActive
 from .serializers import AccountDeleteSerializer, ProfileSerializer
 
--- core/referral/api/v1/views.py
+++ core/referral/api/v1/views.py
@@ -1,11 +1,10 @@
 from accounts.models import Profile, UserType
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, mixins, status, viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 
 from ...models import ReferralRelationship, ReferralToken
 from .filters import ReferralRelationshipFilter, ReferralTokenFilter
