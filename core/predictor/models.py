2883bc100f1ed8b137da2bb46a43a2b8fe72d944
tareqWpy <taregh.khaleghi1381@gmail.com>
2024-12-24 22:50:58 +0000

    refactor all!

--- core/predictor/models.py
+++ core/predictor/models.py
@@ -1,25 +1,16 @@
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.urls import reverse
 
-
-class Predictor(models.Model):
-
-    female_age = models.IntegerField(
-        validators=[MinValueValidator(10), MaxValueValidator(99)]
-    )
-    AMH = models.DecimalField(max_digits=4, decimal_places=2)
-    FSH = models.DecimalField(max_digits=4, decimal_places=2)
-    no_embryos = models.IntegerField()
-    endoendometerial_tickness = models.DecimalField(max_digits=4, decimal_places=2)
-    sperm_count = models.DecimalField(max_digits=4, decimal_places=2)
-    sperm_morphology = models.IntegerField()
-    follicle_size = models.IntegerField()
-    no_of_retreived_oocytes = models.IntegerField()
-    qality_of_embryo = models.IntegerField()
-    quality_of_retreived_oocytes_MI = models.IntegerField()
-    quality_of_retreived_oocytes_MII = models.IntegerField()
-    result = models.IntegerField()
+class Diabetes(models.Model):
+    pregnancies = models.IntegerField()
+    glucose = models.IntegerField()
+    blood_pressure = models.IntegerField()
+    skin_thickness = models.IntegerField()
+    insulin = models.IntegerField()
+    bmi = models.DecimalField(max_digits=4, decimal_places=1)
+    diabetes_pedigree_function = models.DecimalField(max_digits=4, decimal_places=3)
+    age = models.IntegerField()
+    outcome = models.IntegerField()
 
     created_date = models.DateTimeField(auto_now_add=True)
 
